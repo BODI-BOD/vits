e307fd17f5239af366287c336c3eb8c4843be263
BODI-BOD <kingabode2002@gmail.com>
2025-04-22 16:00:04 +0300

    symbols.py

--- text/symbols.py
+++ text/symbols.py
@@ -1,16 +1,6 @@
-""" from https://github.com/keithito/tacotron """
+_pad        = "_"
+_punctuation = "،؛؟!.,…"
+_letters = "ءآأؤإئابةتثجحخدذرزسشصضطظعغفقكلمنهوىي"
+_harakat = "ًٌٍَُِّْ"
 
-'''
-Defines the set of symbols used in text input to the model.
-'''
-_pad        = '_'
-_punctuation = ';:,.!?¡¿—…"«»“” '
-_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-_letters_ipa = "ɑɐɒæɓʙβɔɕçɗɖðʤəɘɚɛɜɝɞɟʄɡɠɢʛɦɧħɥʜɨɪʝɭɬɫɮʟɱɯɰŋɳɲɴøɵɸθœɶʘɹɺɾɻʀʁɽʂʃʈʧʉʊʋⱱʌɣɤʍχʎʏʑʐʒʔʡʕʢǀǁǂǃˈˌːˑʼʴʰʱʲʷˠˤ˞↓↑→↗↘'̩'ᵻ"
-
-
-# Export all symbols:
-symbols = [_pad] + list(_punctuation) + list(_letters) + list(_letters_ipa)
-
-# Special symbol ids
-SPACE_ID = symbols.index(" ")
+symbols = [_pad] + list(_punctuation) + list(_letters + _harakat)
